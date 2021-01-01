91f692fa6563e15d4c21163f3f171f59a68d168e
Ypshir <ypshir@gmail.com>
2021-01-01 12:05:11 +0200

    finish Control component and start Pads component

--- src/App.js
+++ src/App.js
@@ -6,18 +6,12 @@ import './App.css'
 
 function App() {
   const audio = useRef('audio_tag'); // use case for useRef is getting access to DOM nodes. 
-  const toggle = () =>{
-    if(audio.current.paused){
-      audio.current.play()
-    }else{
-      audio.current.pause()
-    }
-  }
   return (
     <div className="App">
       <h1>LOOP MACHINE</h1>
+      <audio onChange={(e)=> console.log(e)} ref={audio} type="audio/mpeg"></audio>
       <Pads />
-      <Controls toggle={toggle} />
+      <Controls />
     </div>
   );
 }
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,20 +1,49 @@
 import React, { useState, useEffect, useRef, useContext } from 'react'
 
-function Controls({toggle}) {
+function Controls() {
     const [playing,setPlaying] = useState(false);
+    const [dur,setDur] = useState(0);
+    const [curr,setCurr] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+    const audio = useRef('audio_tag');
+
+    const fmtMSS = (s) => { return (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + ~~(s) }
+
+    const toggle = () =>{
+        setDur(audio.current.duration)
+        if(audio.current.paused){
+          audio.current.play()
+        }else{
+          audio.current.pause()
+        }
+    }
+    const handleProgress = (e)=>{
+        let compute = (e.target.value*dur)/100;
+        setCurr(compute);
+        audio.current.currentTime= compute;
+    }
+    // useEffect(()=>{
+    //     setDur(audio.current.duration)
+    // },[])
  return(
      <div className="controls container">
+         <audio 
+            onTimeUpdate={(e) => setCurrentTime(e.target.currentTime)}
+            onCanPlay = {(e)=> setDur(e.target.duration)}
+            ref={audio}
+            type="audio/mpeg"
+         />
         <div className="music-controlls">
             <span className="play" onClick={()=>{setPlaying(playing?0:1); toggle();}}>
                 {
-                    playing? <i className="fas fa-pause"></i>:<i className="fas fa-play"></i>
+                    playing ? <i className="fas fa-pause"></i>:<i className="fas fa-play"></i>
                 }
             </span>
         </div>
         <div className="progress">
-            <span className="current-time">1:30</span>
-            <input type="range" name="progresBar" id="progresBar"></input>
-            <span className="end-time">3:19</span>
+            <span className="current-time">{fmtMSS(curr)}</span>
+            <input onChange={handleProgress} value={(curr*100)/dur} type="range" name="progresBar" id="progresBar"></input>
+            <span className="end-time">{fmtMSS(dur)}</span>
         </div>
         <span className="repeat"><i className="fas fa-redo-alt"></i></span>
      </div>
